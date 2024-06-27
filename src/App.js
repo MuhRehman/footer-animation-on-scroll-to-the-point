3917a719e2a51ebbd11db556d1237c7c3fa944bc
Muhammad Rehman <mrehman@broadpeak.pk>
2024-06-27 16:54:37 +0500

    added css styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import SectionOne from './comp/SectionOne';
+import SectionTwo from './comp/SectionTwo';
+import SectionThree from './comp/SectionThree';
+import Footer from './comp/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <SectionOne/>
+       <SectionTwo/>
+       <SectionThree/>
+       <Footer />
     </div>
   );
 }
