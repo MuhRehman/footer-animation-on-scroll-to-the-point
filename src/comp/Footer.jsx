082d192a2b5d33a95a9cba5e4750ed40c1e2d41f
Muhammad Rehman <mrehman@broadpeak.pk>
2024-06-28 16:45:55 +0500

    add the animation funacationaltiy

--- src/comp/Footer.jsx
+++ src/comp/Footer.jsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import React , { useState, useCallback, useEffect } from 'react'
 
 export default function Footer() {
+
+    const [y, setY] = useState(window.scrollY);
+    const [isActive, setisActive] = useState(false);
+
+    const handleNavigation = useCallback(
+      (e) => {
+        const window = e.currentTarget;
+        if (y > window.scrollY) {
+          console.log("scrolling up");
+        } else if (y < window.scrollY) {
+          console.log("scrolling down" + window.scrollY);
+          if (window.scrollY > 2200) {
+            // alert("yes 800 scrolled ");
+            setisActive(true);
+          }
+        }
+        setY(window.scrollY);
+      },
+      [y]
+    );
+  
+    useEffect(() => {
+      setY(window.scrollY);
+      window.addEventListener("scroll", handleNavigation);
+  
+      return () => {
+        window.removeEventListener("scroll", handleNavigation);
+      };
+    }, [handleNavigation]);
+
+
+
   return (
-    <div className='footer-wrap'>Footer</div>
+    <div className='footer-wrap'>
+
+        <h2>Footer</h2>
+     <div className={"container fadein  " + (isActive ? 'visible' : 'hidden')} >
+          <ul>
+            <li>List item 1</li>
+            <li>List item 2</li>
+            <li>List item 3</li>
+          </ul>
+     </div> 
+    </div>
   )
 }
